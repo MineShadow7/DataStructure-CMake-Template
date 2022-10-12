0ddecf869b48fde97e57b09cecb58f9a796b3865
Andrey Karagod <shad-karagodin@ya.ru>
2022-10-12 12:14:25 +0300

    First try on CMake

---
+++ lib_example2/example2.cpp
@@ -0,0 +1,8 @@
+//Copyright 2022 Andrey Karagodin
+
+#include <stdexcept>
+#include "..//lib_example2/example2.h"
+
+float add(float a, float b) {
+	return a + b;
+}
---
+++ lib_example2/example2.h
@@ -0,0 +1,8 @@
+// Copyright 2022 Andrey Karagodin
+
+#ifndef LIB_EXAMPLE_EXAMPLE2_H_
+#define LIB_EXAMPLE_EXAMPLE2_H_
+
+float add(float a, float b);
+
+#endif // !LIB_EXAMPLE_EXAMPLE2_H_
