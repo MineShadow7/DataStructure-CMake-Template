e4eb82e055c5aca9928637b621c703200963ec4b
Andrey Karagodin <shad-karagodin@ya.ru>
2023-04-03 23:43:41 +0300

    Binary Search Tree

--- lib_trees/tree.h
+++ lib_trees/tree.h
@@ -13,63 +13,48 @@ using namespace std;
 
 template <typename T>
 class TNode {
-    T data;
-    TNode* left;
-    TNode* right;
-    TNode* parent;
-
 public:
-    TNode(T _data) {
-        data = _data;
-        left = nullptr;
-        right = nullptr;
-    }
-
-    void setright(TNode* newptr) {
-        cout << data << " -> ";
-    }
-
-    void setleft(TNode* newptr) {
-        cout << data << " -> ";
-    }
+    T data;
+    TNode<T>* left;
+    TNode<T>* right;
+    TNode<T>* parent;
     friend class Tree<T>;
 };
 
 template <typename T>
 class Tree {
-    TNode* root;
-
-    Tree() {
-        root = null;
-    }
-
+    TNode<T>* root;
     bool isEmpty() {
         return root == null;
     }
 protected:
-    TNode* insert(TNode* node, T newdata) {
+    TNode<T>* insert(TNode<T>* node, T data) {
         if (node == NULL)
         {
-            node = new TNode(newdata);
+            TNode<T> *node = new TNode<int>();
+            node->data = data;
+            node->left = NULL;
+            node->right = NULL;
+            node->parent = NULL;
+            return node;
         }
-        else if (node->data < newdata) {
-            node->setright(insert(node->right, newdata));
+        else if (node->data < data) {
+            node->right = insert(node->right, data);
             node->right->parent = node;
         }
         else {
-            node->setleft(insert(node->left, newdata));
+            node->left = insert(node->left, data);
             node->left->parent = node;
         }
-
         return node;
     }
 
-    TNode* Search(TNode* node, T data) {
+    TNode<T>* Search(TNode<T>* node, T data) {
         if (node == NULL) {
             return NULL;
         }
         else if (node->data == data) {
-            return data;
+            return node;
         }
         else if (node->data < data) {
             return Search(node->right, data);
@@ -79,36 +64,94 @@ protected:
         }
     }
 
-    TNode* Remove(TNode* node, T data) {
-        if (node == NULL) {
+    TNode<T>* Remove(TNode<T>* node, T data) {
+        if (node == NULL)
             return NULL;
-        }
+
         // Ќебольшое по€снение: ≈сли лист - спокойно удал€ем.
         if (node->data == data) {
             if (node->left == NULL && node->right == NULL)
                 node = NULL;
+
+            else if (node->left == NULL && node->right != NULL) {
+                // ≈динственный нод который прикреплен снизу будет прикреплен к паренту
+                // сверху вниз: 8-9-10 -> 8-10
+                node->right->parent = node->parent;
+                node = node->right;
+            }
+            else if (node->left != NULL && node->right == NULL) {
+                node->left->parent = node->parent;
+                node = node->left;
+            }
+            else {
+                T successorKey = Successor(data);
+                node->data = successorKey;
+                node->right = Remove(node->right, successorKey);
+            }
         }
-        else if (node->left == NULL && node->right != NULL) {
-            // ≈динственный нод который прикреплен снизу будет прикреплен к паренту
-            // сверху вниз: 8-9-10 -> 8-10
-            node->right->parent = node->parent;
-            node = node->right;
+        else if (node->data < data) {
+            node->right = Remove(node->right, data);
         }
-        else if (node->left != NULL && node->right == NULL) {
-            node->left->parent = node->parent;
-            node = node->left;
+
+        else
+            node->left = Remove(node->left, data);
+        return node;
+    }
+
+    T FindMin(TNode<T>* node) {
+        if (node == NULL)
+            return -1;
+        else if (node->left == NULL)
+            return node->data;
+        else
+            return FindMin(node->left);
+    }
+
+    T FindMax(TNode<T>* node) {
+        if (node == NULL)
+            return -1;
+        else if (node->right == NULL)
+            return node->data;
+        else
+            return FindMax(node->right);
+    }
+
+    T Successor(TNode<T>* node) {
+        // successor это мин. значение правой ветки.
+        if (node->right != NULL)
+        {
+            return FindMin(node->right);
         }
+        //если нет правой ветки
         else {
-            //todo: доделать (https://medium.com/swlh/binary-search-trees-c-484377f0d349)
+            TNode<T>* parentNode = node->parent;
+            TNode<T>* currentNode = node;
+            //≈сли currentNode не рут и имеет правые ветви, то идем выше
+            while ((parentNode != NULL) && (currentNode == parentNode->right)) {
+                currentNode = parentNode;
+                parentNode = currentNode->parent;
+            }
+            //≈сли parentNode != null тогда значение parentNode это successor нода.
+            return parentNode == NULL ? -1 :
+                parentNode->data;
         }
     }
 
-public:
-    void insert(int key) {
-        root = insert(root, key);
+    void print(TNode<T>* node) {
+        if (node == NULL)
+            return;
+        print(node->left);
+        std::cout << node->data << ' ';
+        print(node->right);
     }
-    bool Search(int data) {
-        TNode* res = Search(root, data);
+
+public:
+
+    void insert(T data) {
+        root = insert(root, data);
+    }
+    bool Search(T data) {
+        TNode<T>* res = Search(root, data);
         if (res == NULL) {
             return false;
         }
@@ -116,8 +159,30 @@ public:
             return true;
         }
     }
-    void Remove(int val) {
+    void Remove(T val) {
         root = Remove(root, val);
+    }
+
+    T FindMin() {
+        return FindMin(root);
+    }
+
+    T FindMax() {
+        return FindMax(root);
+    }
+
+    T Successor(T data) {
+        // ≈сть ли вообще этот нод в дереве.
+        TNode<T>* keyNode = Search(root, data);
+
+        // ¬озвращаем значение если найден или -1 если нет.
+        return keyNode == NULL ? -1 :
+            Successor(keyNode);
+    }
+
+    void print() {
+        print(root);
+        std::cout << std::endl;
     }
 };
 
--- main/main.cpp
+++ main/main.cpp
@@ -5,6 +5,67 @@
 #include "../lib_trees/tree.h"
 #include "main.h"
 
-void main() {
+int main() {
+    cout << "Бинарное дерево поиска" << endl;
 
+    Tree<int> *tree = new Tree<int>();
+    // Значения которые вставим в дерево
+    int values[] = { 23, 12, 31, 3, 15, 7, 29, 88, 53 };
+
+    // Вставка значений
+    for (const int& value : values)
+        tree->insert(value);
+
+    // Выписать все значения из дерева
+    cout << "Tree values: ";
+    tree->print();
+
+    // Найти значение 31
+    // Должен быть найден
+    cout << "Search key 31: ";
+    bool b = tree->Search(31);
+    if (b)
+        cout << "found";
+    else
+        cout << "NOT found";
+    cout << endl;
+
+    // Найти значение 18
+    // Должен быть НЕ найден
+    cout << "Search key 18: ";
+    b = tree->Search(18);
+    if (b)
+        cout << "found";
+    else
+        cout << "NOT found";
+    cout << endl;
+
+    // Минимальные и максимальные значения
+    cout << "Min. Key : " << tree->FindMin();
+    cout << endl;
+    cout << "Max. Key : " << tree->FindMax();
+    cout << endl;
+
+
+    // Successor(31) должен быть 53
+    // Successor(15) должен быть 23
+    // Successor(88) должен быть -1 or NULL
+    cout << "Successor(31) = ";
+    cout << tree->Successor(31) << endl;
+    cout << "Successor(15) = ";
+    cout << tree->Successor(15) << endl;
+    cout << "Successor(88) = ";
+    cout << tree->Successor(88) << endl;
+
+    // Удаление
+    cout << "Removing value 31" << endl;
+    tree->Remove(31);
+    cout << "Removing value 15" << endl;
+    tree->Remove(15);
+
+    // Значения 31 и 15 должны были быть удалены
+    cout << "Tree values: ";
+    tree->print();
+
+    return 0;
 }
